09779c475f3b8b610ed9851e9ac301ae4be41a59
wiviw5 <wiviw2221212@gmail.com>
2022-06-07 00:55:26 -0500

    Updates to 0.1.3
    - Cleaned up Code.

--- src/main/java/me/wiviw/chatutils/events/onChatEvent.java
+++ src/main/java/me/wiviw/chatutils/events/onChatEvent.java
@@ -13,26 +13,28 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class onChatEvent {
+
+    private static final Pattern messageStartFriend = Pattern.compile((char) 167 + "aFriend > ", Pattern.LITERAL);
+    private static final Pattern messageStartGuild = Pattern.compile((char) 167 + "2Guild > ", Pattern.LITERAL);
+    private static final Pattern joinPattern = Pattern.compile((char) 167 + "r" + (char) 167 + "ejoined", Pattern.LITERAL);
+
     @SubscribeEvent
     public void onChat(ClientChatReceivedEvent event) {
-        if (event.type==0 || event.type==1) {
+        if (event.type == 0 || event.type == 1) {
             String message = event.message.getFormattedText();
-            Pattern messageStartFriend = Pattern.compile((char) 167 + "aFriend > ", Pattern.LITERAL);
             Matcher friendMatcher = messageStartFriend.matcher(message);
-            Pattern messageStart = Pattern.compile((char) 167 + "2Guild > ", Pattern.LITERAL);
-            Matcher guildMatcher = messageStart.matcher(message);
-            Pattern joinPattern = Pattern.compile((char) 167 + "r" + (char) 167 + "ejoined", Pattern.LITERAL);
+            Matcher guildMatcher = messageStartGuild.matcher(message);
             Matcher joinedMatcher = joinPattern.matcher(message);
             boolean friendMatcherBool = friendMatcher.find();
             boolean guildMatcherBool = guildMatcher.find();
             boolean joinedMatcherBool = joinedMatcher.find();
-            if (friendMatcherBool || guildMatcherBool){
+            if (friendMatcherBool || guildMatcherBool) {
                 int subStringSplit = friendMatcherBool ? 15 : 14;
-                if (joinedMatcherBool){
+                if (joinedMatcherBool) {
                     event.setCanceled(true);
                     IChatComponent comp = new ChatComponentText(message);
                     String user = message.substring(subStringSplit).split(" ")[0];
-                    char userColor = message.charAt(subStringSplit-1);
+                    char userColor = message.charAt(subStringSplit - 1);
                     System.out.println((char) 167 + "dBoop! " + (char) 167 + userColor + user);
                     IChatComponent boopMessage = new ChatComponentText((char) 167 + "dBoop! " + (char) 167 + userColor + user);
                     ChatStyle style = new ChatStyle().setChatClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/boop " + user)).setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, boopMessage));
